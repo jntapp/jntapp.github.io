e0e18d2d49fd78d305cb52350945960d5575e054
phamcuong8868 <phamcuong8868@gmail.com>
2026-02-12 14:36:12 +0700

    upload

---
+++ menu/main.js
@@ -0,0 +1,47 @@
+class Menu
+{
+    constructor(danhSachButton = [])
+    {
+        this.danhSachButton = danhSachButton;
+    }
+
+    render()
+    {
+        const menuContainer = document.createElement('div');
+        menuContainer.className = 'menu';
+
+        this.danhSachButton.forEach(button => 
+        {
+            const buttonElement = button.render(); 
+            menuContainer.appendChild(buttonElement);
+        });
+
+        document.body.appendChild(menuContainer);
+    }
+}
+
+class MenuButton
+{
+    constructor(caption, url)
+    {
+        this.caption = caption;
+        this.url = url;
+    }
+
+    render()
+    {
+        const bt = document.createElement('div');
+        bt.className = 'menu-button';
+        bt.innerHTML = `<a href="${this.url}">${this.caption}</a>`;
+        return bt;
+    }
+}
+
+
+const menu = new Menu([
+    new MenuButton('Home', '/index.html'),
+    new MenuButton('Lịch xe', '/lichxe.html'),
+    new MenuButton('Đóng gói', 'https://jtexpress.vn/vi/quy-cach-dong-goi-hang-hoa')
+]);
+
+menu.render();
