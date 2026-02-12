2ec4d406bae35b8000a2413cf245801d327312b3
phamcuong8868 <phamcuong8868@gmail.com>
2026-02-12 14:47:28 +0700

    edting

---
+++ data/tinh.js
@@ -0,0 +1,37 @@
+class Tinh
+{
+    constructor(name, danhSachTrungTamTrungChuyen, danhSachBuuCuc = [])
+    {
+        this.name = name;
+        this.danhSachTrungTamTrungChuyen = danhSachTrungTamTrungChuyen;
+        this.danhSachBuuCuc = danhSachBuuCuc;
+    }
+}
+
+class BuuCuc
+{
+    constructor(ten, ma)
+    {
+        this.ten = ten;
+        this.ma = ma;
+    }
+}
+
+
+class TrungTamTrungChuyen
+{
+    constructor(ten, ma)
+    {
+        this.ten = ten;
+        this.ma = ma;
+    }
+}
+
+
+const danhSachTinh = [
+    new Tinh('Hà Nội', ['HN01', 'HN02', 'HN03']),
+    new Tinh('Hồ Chí Minh', ['HCM01', 'HCM02', 'HCM03']),
+    new Tinh('Đà Nẵng', ['DN01', 'DN02']),
+    new Tinh('Hải Phòng', ['HP01', 'HP02']),
+    new Tinh('Cần Thơ', ['CT01']),
+];
