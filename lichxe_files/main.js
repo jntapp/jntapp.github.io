9dc58bc650cfc88d86e9aa8935977599f8450266
phamcuong8868 <phamcuong8868@gmail.com>
2026-02-14 09:15:44 +0700

    dffdss

--- lichxe_files/main.js
+++ lichxe_files/main.js
@@ -73,7 +73,7 @@ function loadChiTietXeDiTheoTinh(thoiGianId, tinhId)
     {                
         if (danhSachTrungTamTrungChuyen[i].tinhid === tinhId)
         {
-            danhSachMaTrungTamTrungChuyen.push(danhSachTrungTamTrungChuyen[i].ma);
+            danhSachMaTrungTamTrungChuyen.push(danhSachTrungTamTrungChuyen[i].stt+ '(' + danhSachTrungTamTrungChuyen[i].ma + ')');
         }
     }
     chiTietDiv.innerHTML += danhSachMaTrungTamTrungChuyen.join(' - ');
