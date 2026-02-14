5cecf8e2e2605fe1961a64dc63b70dd25659b154
phamcuong8868 <phamcuong8868@gmail.com>
2026-02-14 08:48:57 +0700

    dffdss

--- data/tttc.js
+++ data/tttc.js
@@ -13,5 +13,8 @@ class TrungTamTrungChuyen
 const danhSachTrungTamTrungChuyen = [
     new TrungTamTrungChuyen(24, '024GW0', 'TTTC Hà Nội 1', 470),
     new TrungTamTrungChuyen(24, '024GW1', 'TTTC Hà Nội 2', 741),
-    new TrungTamTrungChuyen(28, 'TTTC02', 'Trung tâm trung chuyển Hồ Chí Minh 1', 1)
+    new TrungTamTrungChuyen(28, 'TTTC02', 'Trung tâm trung chuyển Hồ Chí Minh 1', 800),
+    new TrungTamTrungChuyen(28, 'TTTC03', 'Trung tâm trung chuyển Hồ Chí Minh 2', 900),
+    new TrungTamTrungChuyen(32, 'TTTC03', 'Trung tâm trung chuyển Nam Định', 300),
+    new TrungTamTrungChuyen(36, 'TTTC04', 'Trung tâm trung chuyển Ninh Bình', 200)
 ];
