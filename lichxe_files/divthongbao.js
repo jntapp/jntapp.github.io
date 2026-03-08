beaab4d53eb95449fce53966ae3d2d73c15d8cfe
phamcuong8868 <phamcuong8868@gmail.com>
2026-03-08 15:16:35 +0700

    dffdss

--- lichxe_files/divthongbao.js
+++ lichxe_files/divthongbao.js
@@ -3,7 +3,7 @@ divThongBao.className = 'thong-bao';
 divThongBao.innerHTML = `<center>
     <div style="position:relative;text-align:left;margin-top:100px;background:white;width:220px;padding:20px;border-radius:5px;">
         <div id="noiDungThongBao"></div>
-        <center><div style="border-radius:5px;border:1px solid rgba(0,0,0,0.3);width:100px;padding:8px;" onclick="divThongBao.style.display='none'">OK</div></center>
+        <center><div style="margin-top:10px;border-radius:5px;border:1px solid rgba(0,0,0,0.3);width:100px;padding:8px;" onclick="divThongBao.style.display='none'">OK</div></center>
     </div></center>
 `;
 document.body.appendChild(divThongBao);
