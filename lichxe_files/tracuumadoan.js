93fb42907f38cbcd50e89aeccdefcf88b07f9ab2
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-03-09 05:54:15 +0700

    Create tracuumadoan.js

---
+++ lichxe_files/tracuumadoan.js
@@ -0,0 +1,16 @@
+const divTraCuu =  document.createElement("div");
+divTraCuu.className = "divTraCuu";
+const inputTimKiem = document.createElement("input");
+inputTimKiem.type = "text";
+inputTimKiem.id = "inputTimKiem";
+inputTimKiem.addEventListener("keyup",function(){
+traCuuMaDoan();
+});
+
+divTraCuu.appendChild(inputTimKiem);
+document.body.appendChild(divTraCuu);
+
+function traCuuMaDoan()
+{
+  let noiDungTimKiem = document.getElementById("inputTimKiem").value;
+}
