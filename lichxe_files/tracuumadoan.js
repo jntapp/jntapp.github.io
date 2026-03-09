442f6e95bc632765d82df651b473b0e8b4d7c820
phamcuong8868 <phamcuong8868@gmail.com>
2026-03-09 13:21:09 +0700

    dffdss

--- lichxe_files/tracuumadoan.js
+++ lichxe_files/tracuumadoan.js
@@ -19,6 +19,7 @@ document.body.appendChild(divTraCuu);
 
 function traCuuMaDoan()
 {
+  console.log("Đang traCuuMaDoan");
   let noiDungTimKiem = document.getElementById("inputTimKiem").value;
   divTraCuu_ketQua.innerHTML = '';
   for (let i =0; i< danhSachTrungTamTrungChuyen.length; i++)
