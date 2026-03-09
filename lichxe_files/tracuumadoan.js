555e54a00f3108814e02b22c0ee1c8e0c922d19b
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-03-10 06:09:55 +0700

    Update tracuumadoan.js

--- lichxe_files/tracuumadoan.js
+++ lichxe_files/tracuumadoan.js
@@ -59,7 +59,7 @@ if (noiDungTimKiem.length > 2)
   {
     if (danhSachTrungTamTrungChuyen[i].ma.indexOf(noiDungTimKiem) !==-1)
     {
-      divTraCuu_ketQua.innerHTML += `<span style="font-size:18px;font-weight:bold;">${danhSachTrungTamTrungChuyen[i].ma}</span><br>
+      divTraCuu_ketQua.innerHTML += `<span style="font-size:22px;font-weight:bold;">${danhSachTrungTamTrungChuyen[i].stt}</span><br><span style="font-size:18px;font-weight:bold;">${danhSachTrungTamTrungChuyen[i].ma}</span><br>
             ${danhSachTrungTamTrungChuyen[i].ten}<br>
             <i>Tuyến: ${danhSachTinh.find(tinh => tinh.id === danhSachTrungTamTrungChuyen[i].tinhid).name}</i><br>`;
         
