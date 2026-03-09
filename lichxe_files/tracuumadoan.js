74be8529fbaf5345251ae2ea402ab01b009ef47f
phamcuong8868 <phamcuong8868@gmail.com>
2026-03-09 13:13:56 +0700

    dffdss

--- lichxe_files/tracuumadoan.js
+++ lichxe_files/tracuumadoan.js
@@ -1,11 +1,15 @@
 const divTraCuu =  document.createElement("div");
 divTraCuu.className = "divTraCuu";
-const inputTimKiem = document.createElement("input");
-inputTimKiem.type = "text";
-inputTimKiem.id = "inputTimKiem";
-inputTimKiem.addEventListener("keyup",function(){
-traCuuMaDoan();
-});
+  const inputTimKiem = document.createElement("input");
+  inputTimKiem.type = "text";
+  inputTimKiem.id = "inputTimKiem";
+  inputTimKiem.addEventListener("keyup",function(){
+  traCuuMaDoan();
+  });
+
+  const divTraCuu_ketQua = document.createElement("div");
+  divTraCuu_ketQua.id = "divTraCuu_ketQua";
+  divTraCuu.appendChild(divTraCuu_ketQua);
 
 divTraCuu.appendChild(inputTimKiem);
 document.body.appendChild(divTraCuu);
@@ -13,12 +17,15 @@ document.body.appendChild(divTraCuu);
 function traCuuMaDoan()
 {
   let noiDungTimKiem = document.getElementById("inputTimKiem").value;
-for (let i =0; i< danhSachTrungTamTrungChuyen.length; i++)
-{
-   if (danhSachTrungTamTrungChuyen[i] == noiDungTimKiem)
-{
-
- break;
-}
-}
+  divTraCuu_ketQua.innerHTML = '';
+  for (let i =0; i< danhSachTrungTamTrungChuyen.length; i++)
+  {
+    if (danhSachTrungTamTrungChuyen[i] == noiDungTimKiem)
+    {
+      divTraCuu_ketQua.innerHTML = `<center><span style="font-size:22px;font-weight:bold;">${danhSachTrungTamTrungChuyen[i].ma}</span><br>
+            ${danhSachTrungTamTrungChuyen[i].ten}<br>
+            <center>Tuyến: ${danhSachTinh.find(tinh => tinh.id === danhSachTrungTamTrungChuyen[i].tinhid).name}</center>`);
+        break;
+    }
+  }
 }
