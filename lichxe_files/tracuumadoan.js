1805b759fd537e1738517c999842910b0574cee8
phamcuong8868 <phamcuong8868@gmail.com>
2026-03-10 14:50:46 +0700

    dffdss

--- lichxe_files/tracuumadoan.js
+++ lichxe_files/tracuumadoan.js
@@ -69,7 +69,7 @@ if (noiDungTimKiem.length > 2)
   {
     if (danhSachTrungTamTrungChuyen[i].ma.indexOf(noiDungTimKiem) !==-1)
     {
-      divTraCuu_ketQua.innerHTML += `<hr style="100%"><span style="font-weight:bold;">${danhSachTrungTamTrungChuyen[i].stt}</span><br><span style="font-size:14px;"><span style="font-weight:bold;">${danhSachTrungTamTrungChuyen[i].ma}</span><br>
+      divTraCuu_ketQua.innerHTML += `<hr style="width:60%;color:rgba(0,0,0,0.3);"><span style="font-weight:bold;">${danhSachTrungTamTrungChuyen[i].stt}</span><br><span style="font-size:14px;"><span style="font-weight:bold;">${danhSachTrungTamTrungChuyen[i].ma}</span><br>
             ${danhSachTrungTamTrungChuyen[i].ten}<br>
             <i>Tuyến: ${danhSachTinh.find(tinh => tinh.id === danhSachTrungTamTrungChuyen[i].tinhid).name}</i></span><br>`;
         
