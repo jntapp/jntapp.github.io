bfae64f7a12b3cc83036b750394db6446aa00711
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-03-18 22:41:24 +0700

    Update tinh.js

--- data/tinh.js
+++ data/tinh.js
@@ -7,28 +7,7 @@ class Tinh
     }
 }
 
-const danhSachTinh = [
-    new Tinh(14, 'Quảng Ninh'),
-    new Tinh(15, 'Hải Phòng'),
-    new Tinh(18, 'Nam Định'),
-    new Tinh(24, 'Hà Nội'),
-    new Tinh(241, 'Long Biên'),
-
-    new Tinh(28, 'Hồ Chí Minh'),    
-    new Tinh(35, 'Ninh Bình'),
-    new Tinh(36, 'Thanh Hóa'),
-    new Tinh(37, 'Nghệ An'),
-    new Tinh(41, 'Hưng Yên'),
-    new Tinh(56, 'Đà Nẵng'),
-    new Tinh(63, 'Quảng Ngãi'),
-    new Tinh(64, 'Tây Nguyên'),
-    new Tinh(70, 'Đồng Nai'),
-    new Tinh(74, 'Bình Dương'),
-    new Tinh(80, 'Long Hậu'),
-    new Tinh(83, 'Cần Thơ'),
-    new Tinh(90, 'Long An'),
-    new Tinh(99, 'Bắc Ninh')
-];
+//const danhSachTinh = [new Tinh(14, 'Quảng Ninh'),
 
 
 
