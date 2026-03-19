5d3eedaca4ab3b69d22b6ffb219406bafe944df8
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-03-19 17:21:14 +0700

    Update main.js

--- webapp/user/login/main.js
+++ webapp/user/login/main.js
@@ -1,1 +1,15 @@
-
+function doLogin()
+{
+  let userName = document.getElementById("userName");
+  let pass = document.getElementById("passWord");
+  let loginInfor = {userName: userName; passWord: pass};
+  google.script.run.withSuccessHandler(function(status) 
+  {
+    status = JSON.parse(status);
+    if (status.status == 'ok')
+    {
+      localStorage.setItem("userKey", status.userKey);
+      window.location.href = "#?page=index";
+    }
+  }).doLogin();   
+}
