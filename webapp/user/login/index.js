14499a98dcf7c08bf041670afe44c0daba3c1367
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-03-20 14:01:11 +0700

    Update index.js

--- webapp/user/login/index.js
+++ webapp/user/login/index.js
@@ -9,6 +9,59 @@ class DangNhap
   {
     const login_container = document.creatElement('div');
     login_container.className = "login__container";
+      const login_main = document.createElement('div');
+      login_main.className = 'login__main';
+      login_main.innerHTML = `
+        <table class="login__header">
+          <tr>
+              <td style="position:relative;">
+              <div style="width:100%;text-align:center;">
+                  <img src="https://jntapp.github.io/webapp/user/login/contact_caption.svg" style="height:25px;opacity:0.75;">
+              </div>
+              
+              <img src="https://jntapp.github.io/webapp/user/login/contact_book.svg" style="height:32px;position:absolute;right:30px;top:2px;opacity:0.35;">
+              </td>
+          </tr>
+        </table>
+        <div class="DivUnderHeader" style="height:70px;">
+        </div>
+        
+        <font color="white">
+        <div class="error">Vui lòng đăng nhập để vào trang này <br> Please login to access this page</div></font>
+        
+        <div>
+          <table style="width:100%;padding:8px;" border="0">
+            <tr>
+              <td>
+              </td>
+            </tr>
+            <tr>
+                <td style="position:relative;">
+                    <input type="text" name="username" id="userName" placeholder="Tên đăng nhập" class="InputBox InputA" value="">
+                    <img src="https://jntapp.github.io/webapp/user/login/user_icon.png" style="position:absolute;left:5px;bottom:7px;width:18px;opacity:0.5;">
+                </td>
+              </tr>
+      
+            <tr>
+                <td style="position:relative;">
+                    <input type="password" name="password" id="passWord" placeholder="Mật khẩu" class="InputBox InputA">
+                    <img src="https://jntapp.github.io/webapp/user/login/password_icon.png" style="position:absolute;left:5px;bottom:7px;width:18px;opacity:0.5;">
+                </td>
+              </tr>
+            <tr>
+              <td style="padding-top:35px;text-align:center;">
+                <button name="login_user" 
+                onclick = "doLogin();"
+                style="background:#009933;color:white;padding:12px;width:200px;border-radius:5px;border:1px solid rgba(255,255,255,0.5);">Đăng nhập</button>
+              </td>
+            </tr>
+          </table>
+          <div style="height:50px;">
+            <p style="display:none;">
+              
+            </p>
+        </div>`;
+      login_container.appendChild('login_main');
       
   }
 }
