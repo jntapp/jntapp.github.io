f53fd1c976626a0d275a9de4bab2ca4cebc43e01
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-03-20 17:48:57 +0700

    Update checkUser.js

--- webapp/user/checkUser.js
+++ webapp/user/checkUser.js
@@ -2,17 +2,13 @@ var status = {status: 'notOk', userKey: '0', message:''};
 function checkUser()
 {
   let key = localStorage.getItem("userKey");
-  alert(key);
   if (key)
   {
     google.script.run.withSuccessHandler(function(user) 
     {
       user = JSON.parse(user);
       status = user;
-      if (user.status == 'ok')
-      {
-      }
-      else
+      if (user.status !== 'ok')
       {
         showLogin();
       }
