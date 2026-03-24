a7aa1ed0dd05490f96ef9a2c381ef8b73f4afd4e
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-03-24 14:42:43 +0700

    Delete webapp/user/logout.js

--- webapp/user/logout.js
+++
@@ -1,8 +0,0 @@
-function logout()
-{
-  var status = {userHoVaTen : '', userKey: '', status: 'notOk', massage: ''};
-  localStorage.setItem("userKey", '');
-  localStorage.setItem('loginStatus', 'notOk');
-  localStorage.setItem("userHoVaTen", '');
-  document.getElementById('userMenu_hoVaTen').innerText = '-';
-}
