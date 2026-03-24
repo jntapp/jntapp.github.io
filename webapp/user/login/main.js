c035acfe6ba05ad90653c72b30125523da850e2e
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-03-24 16:08:11 +0700

    Create main.js

---
+++ webapp/user/login/main.js
@@ -0,0 +1,46 @@
+var status = {userHoVaTen: '', status: 'notOk', userKey: '0', message:''};
+
+function doLogin()
+{
+  let userName = document.getElementById("userName").value;
+  let pass = document.getElementById("passWord").value;
+  if (userName.length < 3)
+  {
+    alert('Vui lòng kiểm tra tên đăng nhập!');
+  }
+  else if (pass.length < 3)
+  {
+    alert('Vui lòng kiểm tra lại mật khẩu!');
+  }
+  else
+  {
+    let loginInfor = {userName: userName, passWord: pass};
+    google.script.run.withSuccessHandler(function(status) 
+    {
+      status = JSON.parse(status);
+      if (status.status == 'ok')
+      {
+        localStorage.setItem("userKey", status.userKey);
+        localStorage.setItem("loginStatus", 'ok');
+        localStorage.setItem("userHoVaTen", status.userHoVaTen);
+        document.getElementById('menuUser_hoVaTen').innerText = status.userHoVaTen;
+        showLoginSuccessfull();
+      }
+      else
+      {
+        alert(status.message);
+      }
+    }).doLogin(JSON.stringify(loginInfor));
+  }  
+}
+
+function showLoginSuccessfull()
+{
+  document.getElementById('loginError').style.display = 'none';
+  document.getElementById('loginForm').style.display = 'none';
+  document.getElementById('loginSuccessfull').style.display = 'block';
+  
+  setTimeout(function(){
+    document.getElementById('loginContainer').style.display = 'none';
+  },500);
+}
