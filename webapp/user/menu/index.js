b59fa6212465c1e8e794f46ff041d453d413aa59
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-03-24 17:37:56 +0700

    Update index.js

--- webapp/user/menu/index.js
+++ webapp/user/menu/index.js
@@ -15,7 +15,7 @@ menuUser.innerHTML = `
                   </a>
             </div>
             <div>
-                  <span style="margin-botton:20px;" id="userMenu_dangXuat" onclick="logout();" href="javascript:void(0);" class="menu__button">
+                  <span style="margin-botton:20px;" id="userMenu_dangXuat" onclick="logout();" class="menu__button">
                         <font color="white">Đăng xuất</font>
                   </span>
             </div>`;
