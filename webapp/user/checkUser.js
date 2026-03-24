c95960acc6fdbc1042df7965f8fdcbdb86260ff9
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-03-24 14:14:16 +0700

    Update checkUser.js

--- webapp/user/checkUser.js
+++ webapp/user/checkUser.js
@@ -1,15 +1,17 @@
-var status = {status: 'notOk', userKey: '0', message:''};
+var status = {userHoVaTen: '', status: 'notOk', userKey: '0', message:''};
 function checkUser()
 {
   let key = localStorage.getItem("userKey");
-  console.log(key);
+  //console.log(key);
   if (key)
   {
     google.script.run.withSuccessHandler(function(user) 
     {
       user = JSON.parse(user);
       localStorage.setItem('loginStatus', user.status);
-      console.log(user);
+      localStorage.setItem("userHoVaTen", status.userHoVaTen);
+      document.getElementById('menuUser_hoVaTen').value = status.userHoVaTen;
+      //console.log(user);
       status = user;
       if (user.status !== 'ok')
       {
@@ -19,7 +21,7 @@ function checkUser()
   }
   else
   {
-    console.log('key khong ton tai');
+    //console.log('key khong ton tai');
     localStorage.setItem('loginStatus', 'notOk');
     showLogin();
   }
