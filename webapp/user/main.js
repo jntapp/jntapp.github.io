a3bbb1add3327b6c346b486b8f45abf42c398852
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-03-24 14:42:04 +0700

    Create main.js

---
+++ webapp/user/main.js
@@ -0,0 +1,11 @@
+
+
+
+function logout()
+{
+  var status = {userHoVaTen : '', userKey: '', status: 'notOk', massage: ''};
+  localStorage.setItem("userKey", '');
+  localStorage.setItem('loginStatus', 'notOk');
+  localStorage.setItem("userHoVaTen", '');
+  document.getElementById('userMenu_hoVaTen').innerText = '-';
+}
