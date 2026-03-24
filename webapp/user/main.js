842b8af20a259bec36d96d1104ae3e2f49d1d59c
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-03-24 17:16:52 +0700

    Update main.js

--- webapp/user/main.js
+++ webapp/user/main.js
@@ -31,6 +31,13 @@ function checkUser()
 
 checkUser();
 
+document.getElementById('userMenu_dangXuat').addEventListener('click',
+  function(e)
+  {
+    e.preventDefault();
+    logout();
+  }
+);
 
 function logout()
 {
